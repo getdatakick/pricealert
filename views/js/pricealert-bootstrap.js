3046670c229ce8ca10ac3945e8ac07e3d58b4c06
getdatakick <petr@getdatakick.com>
2018-02-14 14:15:44 +0100

    fix boostrap

--- views/js/pricealert-bootstrap.js
+++ views/js/pricealert-bootstrap.js
@@ -1,17 +1,29 @@
 (function() {
-  window.priceAlertUrl = window.priceAlertUrl.replace('http:', window.location.protocol);
-  window.showPriceAlert = false;
-  window.PriceAlert = function(show) {
-    if (show) {
-      window.showPriceAlert = true;
-      console.warn('Price alert deferred');
+  var bootstrapPriceAlert = function() {
+    window.priceAlertUrl = window.priceAlertUrl.replace('http:', window.location.protocol);
+    window.showPriceAlert = false;
+    window.PriceAlert = function(show) {
+      if (show) {
+        window.showPriceAlert = true;
+        console.warn('Price alert deferred');
+      }
     }
+    var src = window.priceAlertUrl.replace('ajax.php', 'views/js/pricealert.js');
+    var tag = document.createElement('script');
+    tag.src = src;
+    tag.setAttribute('defer','');
+    tag.setAttribute('async','');
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  };
+
+  if (document.readyState === 'complete') {
+    bootstrapPriceAlert();
+  } else {
+    document.addEventListener('readystatechange',  () => {
+      if (document.readyState === 'complete') {
+        bootstrapPriceAlert();
+      }
+    });
   }
-  var src = window.priceAlertUrl.replace('ajax.php', 'views/js/pricealert.js');
-  var tag = document.createElement('script');
-  tag.src = src;
-  tag.setAttribute('defer','');
-  tag.setAttribute('async','');
-  var firstScriptTag = document.getElementsByTagName('script')[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 })();
